1706fc497a7a3015d5e8358da7063ae8dbf02e7a
divukman <divukman@gmail.com>
2020-10-16 23:39:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+ <v-app>
+    <v-toolbar app>
+
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">NXDemo</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>
+          <router-link to="/">Home</router-link>
+        </v-btn>
+        
+	<v-btn flat>
+          <router-link to="/about">About</router-link>
+        </v-btn>
+
+        <v-btn flat>
+          <router-link to="/agents">Agents</router-link>
+        </v-btn>
+      </v-toolbar-items>
+
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Agents.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          NXDemo AGENTS
+        </h1>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Agents',
+
+    data: () => ({
+    })
+  }
+</script>
